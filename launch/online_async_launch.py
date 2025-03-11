5b9047997eae916d9941a989b3206a90ba1ace84
harrietkr <harrietlee.kr@gmail.com>
2025-03-11 20:29:01 +0000

    Add tf broadcaster for laser and camer frames

--- launch/online_async_launch.py
+++ launch/online_async_launch.py
@@ -30,11 +30,19 @@ def generate_launch_description():
         executable='async_slam_toolbox_node',
         name='slam_toolbox',
         output='screen')
+    
+    # Start TF Static Broadcaster Node
+    start_tf_broadcaster_node = Node(
+        package='slam_toolbox',
+        executable='tf_broadcaster_node.py',
+        name='tf_broadcaster',
+        output='screen')
 
     ld = LaunchDescription()
 
     ld.add_action(declare_use_sim_time_argument)
     ld.add_action(declare_slam_params_file_cmd)
     ld.add_action(start_async_slam_toolbox_node)
+    ld.add_action(start_tf_broadcaster_node)
 
     return ld
